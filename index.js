3a03cc13a0bb9f6acdf111b2bfbb4f33189ac1e0
abdnimit1203 <abdullah.ibneali152pro@gmail.com>
2023-11-07 05:44:32 +0600

    Id wise single data get + delete one option added

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-const express  = require("express")
-const cors = require('cors');
-require('dotenv').config()
-const assigments = require('./dummy.json');
-const app = express()
-const port = process.env.PORT || 3000
-
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const assigments = require("./dummy.json");
+const app = express();
+const port = process.env.PORT || 3000;
 
 // middleware
 
-app.use(cors())
-app.use(express.json())
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.SECRET_USER}:${process.env.SECRET_PASSWORD}@cluster0.di78vms.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -21,26 +24,42 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-
     await client.connect();
 
-    const assignmentCollection = client.db("collaboraTaskDB").collection("assignments");
+    const assignmentCollection = client
+      .db("collaboraTaskDB")
+      .collection("assignments");
 
-    app.get('/api/v1/assignments', async(req,res)=>{
-        const cursor = assignmentCollection.find()
-        const result = await cursor.toArray();
-      res.send(result)
-    })
-    app.post('/api/v1/user/create-assignments', async(req,res)=>{
-        const assigmentsData = req.body
-        const result = await assignmentCollection.insertOne(assigmentsData)
-        res.send(result);
-    })
+    app.get("/api/v1/assignments", async (req, res) => {
+      const cursor = assignmentCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    //id wise assignment get
+    app.get("/api/v1/assignments/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const assignment = await assignmentCollection.findOne(query);
+      res.send(assignment);
+    });
+    app.post("/api/v1/user/create-assignment", async (req, res) => {
+      const assigmentsData = req.body;
+      const result = await assignmentCollection.insertOne(assigmentsData);
+      res.send(result);
+    });
+    //id wise assignment delete
+    app.delete("/api/v1/assignments/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await assignmentCollection.deleteOne(query);
+      res.send(result);
+    });
+
 
 
 
@@ -51,7 +70,9 @@ async function run() {
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -59,25 +80,22 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Collabora Task server is running...");
+});
 
-
-
-
-app.get('/', (req,res)=>{
-    res.send("Collabora Task server is running...")
-})
-
-app.get('/assignments', (req,res)=>{
-    res.send(assigments)
-})
+app.get("/assignments", (req, res) => {
+  res.send(assigments);
+});
 // id wise assignment
-app.get('/assignments/:id', (req,res)=>{
-    const id = parseInt(req.params.id )
-    console.log(id);
-    const assignment = assigments.find(assignment=>assignment.id === id )|| {}
-    res.send(assignment)
-})
+app.get("/assignments/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  console.log(id);
+  const assignment =
+    assigments.find((assignment) => assignment.id === id) || {};
+  res.send(assignment);
+});
 
-app.listen(port, ()=>{
-    console.log(`CollaboraTask server running on PORT: ${port}`);
-})
+app.listen(port, () => {
+  console.log(`CollaboraTask server running on PORT: ${port}`);
+});
