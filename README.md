719e276c550cebed694c83b82fc560e21b6ba0e2
abdnimit1203 <abdullah.ibneali152pro@gmail.com>
2023-11-06 23:23:23 +0600

    mongoDb connection setup + env variables used + asignments api get and post created

---
+++ README.md
@@ -0,0 +1,10 @@
+# SERVER SIDE : CollaboraTask  
+
+b8a11
+Backend server side repo for **CollaboraTask**
+
+For this server the Database i used is MongoDB. CRUD Operations are handled in MongoDB string here!
+
+- Express.js is used for JSON and REST API operations.
+
+[CLIENT SIDE SERVER IS HERE](https://github.com/Porgramming-Hero-web-course/b8a11-client-side-abdnimit1203)
--- index.js
+++ index.js
@@ -12,6 +12,55 @@ app.use(cors())
 app.use(express.json())
 
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.SECRET_USER}:${process.env.SECRET_PASSWORD}@cluster0.di78vms.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+    await client.connect();
+
+    const assignmentCollection = client.db("collaboraTaskDB").collection("assignments");
+
+    app.get('/api/v1/assignments', async(req,res)=>{
+        const cursor = assignmentCollection.find()
+        const result = await cursor.toArray();
+      res.send(result)
+    })
+    app.post('/api/v1/user/create-assignments', async(req,res)=>{
+        const assigmentsData = req.body
+        const result = await assignmentCollection.insertOne(assigmentsData)
+        res.send(result);
+    })
+
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
 
 
 app.get('/', (req,res)=>{
